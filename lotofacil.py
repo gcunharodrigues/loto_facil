abe19e6147164ebb985178be68d8bd002efe9b7a
gcunharodrigues <gcunharodrigues@yahoo.es>
2021-10-15 07:56:22 -0300

    Data added

--- lotofacil.py
+++ lotofacil.py
@@ -4,4 +4,5 @@ filename = 'data/loto_facil_compiled_draws_2345.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header = next(reader)
-    print(header)
+    data = [row for row in reader]
+    print(data)
